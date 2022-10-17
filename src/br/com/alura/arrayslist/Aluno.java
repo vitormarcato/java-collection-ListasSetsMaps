a667ddbe4500979c03e8ecc8b4792193bdefb31c
Vitor Marcato <vitor.marcatof@gmail.com>
2022-10-17 13:58:03 -0300

    aula 7

--- src/br/com/alura/arrayslist/Aluno.java
+++ src/br/com/alura/arrayslist/Aluno.java
@@ -5,6 +5,9 @@ public class Aluno {
 	private int numeroMatricula;
 
 	public Aluno(String nome, int numeroMatricula) {
+		if(nome == null) {
+			throw new NullPointerException("nome não pode ser nulo");
+		}
 		this.nome = nome;
 		this.numeroMatricula = numeroMatricula;
 	}
@@ -20,5 +23,16 @@ public class Aluno {
 	@Override
 	public String toString() {
 		return "[Aluno: " + this.nome + " Matrícula: " + this.numeroMatricula + "]";
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		Aluno outro = (Aluno) obj;
+		return this.nome.equals(outro.nome);
+	}
+	
+	@Override
+	public int hashCode() {
+		return this.nome.hashCode();
 	}
 }
--- src/br/com/alura/arrayslist/Curso.java
+++ src/br/com/alura/arrayslist/Curso.java
@@ -55,4 +55,7 @@ public class Curso {
 		return Collections.unmodifiableSet(alunos);
 	}
 
+	public boolean estaMatriculado(Aluno aluno) {
+		return this.alunos.contains(aluno);
+	}
 }
